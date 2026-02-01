83a27cf37217a091a6f3719e27b9e9524e16ca1e
sachin_07 <sachin@adspostx.com>
2026-02-01 14:55:43 +0530

    Update hotel.module.ts

--- src/hotels/hotel.module.ts
+++ src/hotels/hotel.module.ts
@@ -5,7 +5,6 @@ import { Hotel } from 'src/entities/hotel.entity';
 import { Room } from 'src/entities/room.entity';
 import { RoomType } from 'src/entities/room_type.entity';
 import { RoomTypeInventory } from 'src/entities/room_type_inventory.entity';
-
 import { CreateHotelController } from './controllers/create-hotel.controller';
 import { GetHotelsController } from './controllers/get-hotels.controller';
 import { HotelByIdController } from './controllers/hotel-by-id.controller';
